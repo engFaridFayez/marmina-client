879f417299653f52137f1e99895dbb4d7e5a702e
Farid <faridfaiz1@outlook.com>
2026-01-09 12:01:44 +0200

    lol1

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,15 +1,14 @@
 import { createStore } from 'vuex'
-import administraionManagement from './administrationManagement'
+import state from './state'
+import administrationManagement from './administrationManagement'
+import { GlobalState } from './types'
 
-export default createStore({
-  state: {
-  },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
+const store = createStore<GlobalState>({
+  state,
+  modules:{
+    administrationManagement
   }
 })
+
+
+export default store;
--- src/views/adminstration/AdminView.vue
+++ src/views/adminstration/AdminView.vue
@@ -1,12 +1,17 @@
 <template>
-    <h1>Hello Admin!!</h1>
+    <div>
+        <users-overview class="users"></users-overview>
+    </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import UsersOverview from './screens/UsersOverview.vue';
 
 export default defineComponent({
-    components:{},
+    components:{
+        UsersOverview
+    },
     props:{},
     emits:{},
 
@@ -18,5 +23,13 @@ export default defineComponent({
 
 <style lang="scss" scoped>
 
+.users {
+    position: absolute;
+    height: 30%;
+    bottom: 20px;
+    width: 78%;
+    margin: .5rem .5rem 1rem;
+    overflow-x: scroll;
+}
 
 </style>
